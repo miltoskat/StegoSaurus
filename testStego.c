d2aae6ef10270e394fe23ac6dd36e8af6ed4a19c
miltoskat <miltoskat@hotmail.com>
2016-08-11 16:33:07 +0300

    Deny-if-mismatch

--- testStego.c
+++ testStego.c
@@ -5,7 +5,7 @@
 
 int main(){
   FILE *f,*pic;
-  int c=0,flag=0;
+  int c=0,flag=0,denied=0;
   char oa[1000];
   char ob[1000];
   unsigned int bufferF,bufferP;
@@ -32,13 +32,18 @@ while(!feof(f)){
     c++;
     fread(&bufferP,sizeof(char),1,pic);
     if(bufferF==bufferP){
-      //printf("\n Match found");
       printf("\n File hex: %02x   Picture hex: %02x   counter= %d   counter hex: %02x",bufferF,bufferP,c,c);
       flag=1;
     }
   }
-  if (flag==0)
-  printf("NO MATCH FOUND FOR THIS BIT: %02x",bufferF);
+  if (flag==0){
+    printf("\n  File hex: %02x   NO MATCH FOUND",bufferF);
+    denied++;
+  }
+  if (denied>0){
+    printf("\n\n Try a larger/more diverse file\n");
+    return -1;
+  }
   fseek(pic, SEEK_SET, 0);
   flag=0;
   c=0;
