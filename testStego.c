1a1f82afa8c469c20e5b4797c537681120a5a305
miltoskat <miltoskat@hotmail.com>
2016-08-11 15:26:39 +0300

    Added !feof print check
    It’s pretty straightforward it just checks if the file is in the feof
    stage so it won’t reprint the last value in buffer.

--- testStego.c
+++ testStego.c
@@ -16,6 +16,7 @@ int main(){
   printf("\n File successfully opened\n");
 while(!feof(g)){
   fread(&buffer,sizeof(char),1,g);
+  if(!feof(g)){
   printf("%02x", buffer);
   c1++;
   c2++;
@@ -27,6 +28,7 @@ while(!feof(g)){
     c2=0;
     printf("\n");
   }
+}
 }
 printf("\n");
 return(0);
