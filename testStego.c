be7240a67abbdbbc843f838f0246707c6e985106
miltoskat <miltoskat@hotmail.com>
2016-08-11 14:46:45 +0300

    2-file-open
    Added 2 file open in program which searches for matches inside the
    file(doesn’t work right yet)

--- testStego.c
+++ testStego.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define KGRN  "\x1B[32m"
+#define KWHT  "\x1B[37m"
 
 int main(){
-  FILE *g;
-  int c1=0,c2=0;
+  FILE *g,*pic;
+  int c=0,flag=0;
   char oa[1000];
-  unsigned int buffer;
-  printf("\n Please give path of picture: ");
+  char ob[1000];
+  unsigned int buffer,buffer1;
+  printf("\n Please give path of the file: ");
   gets(oa);
   g=fopen(oa,"rb+");
   if(!g){
@@ -14,19 +17,29 @@ int main(){
     return -1;
   }else
   printf("\n File successfully opened\n");
+  printf("\n Please give the path of the picture: ");
+  gets(ob);
+  pic=fopen(ob,"rb+");
+  if(!pic){
+    printf("\n File not found");
+    return -1;
+  }else
+  printf("\n File successfully opened\n");
+
 while(!feof(g)){
-  fread(&buffer,sizeof(char),1,g);
-  printf("%02x", buffer);
-  c1++;
-  c2++;
-  if(c1==2){
-    c1=0;
-    printf(" ");
+  fread(&buffer1,sizeof(char),1,g);
+  printf("\n Outer counter print c=%d",c);
+  while(!feof(pic) && flag==0){
+    c++;
+    fread(&buffer,sizeof(char),1,pic);
+    if(buffer1==buffer){
+      printf("\n Match found");
+      printf("\n %sInner counter print %sc=%d",KGRN,KWHT,c);
+      flag=0;
+    }
   }
-  if (c2==16){
-    c2=0;
-    printf("\n");
-  }
+  flag=0;
+  c=0;
 }
 printf("\n");
 return(0);
