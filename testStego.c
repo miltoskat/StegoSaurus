2084a5b9dc5e9f1569cb3880208cae91bd169d85
miltoskat <miltoskat@hotmail.com>
2016-08-11 16:55:17 +0300

    Minnor fixes

--- testStego.c
+++ testStego.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define KGRN  "\x1B[32m"
-#define KWHT  "\x1B[37m"
+#define KWHT  "\x1b[0m"
 
 int main(){
   FILE *f,*pic;
@@ -16,7 +16,7 @@ int main(){
     printf("\n File not found");
     return -1;
   }else
-  printf("\n File successfully opened\n");
+  printf(" %s------------------------\n %sFile successfully opened\n %s------------------------%s",KGRN,KWHT,KGRN,KWHT);
   printf("\n Please give the path of the picture: ");
   gets(ob);
   pic=fopen(ob,"rb+");
@@ -24,26 +24,28 @@ int main(){
     printf("\n File not found");
     return -1;
   }else
-  printf("\n File successfully opened\n");
+  printf(" %s------------------------\n %sFile successfully opened\n %s------------------------%s",KGRN,KWHT,KGRN,KWHT);
 
 while(!feof(f)){
   fread(&bufferF,sizeof(char),1,f);
-  while(!feof(pic) && flag==0){
-    c++;
-    fread(&bufferP,sizeof(char),1,pic);
-    if(bufferF==bufferP){
-      printf("\n File hex: %02x   Picture hex: %02x   counter= %d   counter hex: %02x",bufferF,bufferP,c,c);
-      flag=1;
+  if(!feof(f)){
+    while(!feof(pic) && flag==0){
+      c++;
+      fread(&bufferP,sizeof(char),1,pic);
+      if(bufferF==bufferP){
+        printf("\n File hex: %02x   Picture hex: %02x   counter= %d   counter hex: %02x",bufferF,bufferP,c,c);
+        flag=1;
+      }
     }
+    if (flag==0){
+      printf("\n  File hex: %02x   NO MATCH FOUND",bufferF);
+      printf("\n\n Try a larger/more diverse file\n\n");
+      return -1;
+    }
+    fseek(pic, SEEK_SET, 0);
+    flag=0;
+    c=0;
   }
-  if (flag==0){
-    printf("\n  File hex: %02x   NO MATCH FOUND",bufferF);
-    printf("\n\n Try a larger/more diverse file\n");
-    return -1;
-  }
-  fseek(pic, SEEK_SET, 0);
-  flag=0;
-  c=0;
 }
 printf("\n");
 return(0);
